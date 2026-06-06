e7a3098cc556c4741ceb0ac44d2346d694934942
Chris Lewis <me@chrislewis.au>
2026-06-06 07:55:37 -0400

    task-runner(match-libultra): fix '- [0xA6E80, asm] # ultra:settimer.c'

---
+++ src/ultra/os/settimer.c
@@ -0,0 +1,20 @@
+#include "PR/os_internal.h"
+#include "PRinternal/osint.h"
+
+int osSetTimer(OSTimer *t, OSTime countdown, OSTime interval, OSMesgQueue *mq, OSMesg msg) {
+    OSTime time;
+
+    t->next = NULL;
+    t->prev = NULL;
+    t->interval = interval;
+    t->value = (countdown != 0) ? countdown : interval;
+    t->mq = mq;
+    t->msg = msg;
+
+    time = __osInsertTimer(t);
+    if (__osTimerList->next == t) {
+        __osSetTimerIntr(time);
+    }
+
+    return 0;
+}
